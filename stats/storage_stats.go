6f309a588dddac60961de1d05a6c762632dc4f19
Tim Hynes <mrtimhynes@googlemail.com>
2020-01-27 20:11:02 +0100

    updated functions in storage_stats module

--- stats/storage_stats.go
+++ stats/storage_stats.go
@@ -6,13 +6,34 @@ import (
     "encoding/json"
 )
 
+// RunwayBody - return interface for runway remaining
+type RunwayBody struct {
+	clusterName string
+	days		float64
+}
+
+// StorageSummaryBody - return interface for storage summary
+type StorageSummaryBody struct {
+	clusterName 	string
+	available		float64
+	liveMount		float64
+	miscellaneous	float64
+	snapshot		float64
+	total			float64
+	used			float64
+}
+
 // GetRunwayRemaining ...
-func GetRunwayRemaining(rubrik *rubrikcdm.Credentials) string {
+func GetRunwayRemaining(rubrik *rubrikcdm.Credentials, clustername string) string {
 	runwayRemaining,err := rubrik.Get("internal","/stats/runway_remaining")
 	if err != nil {
 		log.Fatal(err)
 	}
-	json, err := json.Marshal(runwayRemaining)
+	response := &RunwayBody{
+		clusterName: 	clustername,
+		days:  			runwayRemaining.(map[string]interface{})["days"].(float64),
+	}
+	json, err := json.Marshal(response)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -20,12 +41,21 @@ func GetRunwayRemaining(rubrik *rubrikcdm.Credentials) string {
 }
 
 // GetStorageSummary ...
-func GetStorageSummary(rubrik *rubrikcdm.Credentials) string {
+func GetStorageSummary(rubrik *rubrikcdm.Credentials, clustername string) string {
 	storageStats,err := rubrik.Get("internal","/stats/system_storage")
 	if err != nil {
 		log.Fatal(err)
 	}
-	json, err := json.Marshal(storageStats)
+	response := &StorageSummaryBody{
+		clusterName: 	clustername,
+		available:		storageStats.(map[string]interface{})["available"].(float64),
+		liveMount:		storageStats.(map[string]interface{})["liveMount"].(float64),
+		miscellaneous:	storageStats.(map[string]interface{})["miscellaneous"].(float64),
+		snapshot:		storageStats.(map[string]interface{})["snapshot"].(float64),
+		total:			storageStats.(map[string]interface{})["total"].(float64),
+		used:			storageStats.(map[string]interface{})["used"].(float64),
+	}
+	json, err := json.Marshal(response)
 	if err != nil {
 		log.Fatal(err)
 	}
